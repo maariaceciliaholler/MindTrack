d9d195ec71ac12073b761b272d94bcf3b3c44c2e
Luiza Nurnberg <nurnbergluiza@gmail.com>
2023-08-19 17:40:43 -0300

    feat(project): add project basic configuration

---
+++ mindtrack/src/main/java/com/devmindtrack/mindtrack/MindtrackApplication.java
@@ -0,0 +1,13 @@
+package com.devmindtrack.mindtrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindtrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindtrackApplication.class, args);
+	}
+
+}
---
+++ mindtrack/src/test/java/com/devmindtrack/mindtrack/MindtrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmindtrack.mindtrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindtrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
